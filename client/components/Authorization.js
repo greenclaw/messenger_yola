12fd2531be27d70cf6e54e90b76f6a0d5eeb80a4
Roman Anderson <greenclawanderson@gmail.com>
2018-04-07 16:15:02 +0300

    Add authentication skeleton

--- client/components/Authorization.js
+++ client/components/Authorization.js
@@ -1,13 +1,59 @@
 import React from 'react';
-import {Link} from 'react-router';
+var firebase = require('../firebase.js');
+import { Link } from 'react-router';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 
+var googleProvider = new firebase.auth.GoogleAuthProvider();
 
 class Authorization extends React.Component{
+
+  constructor(props) {
+    super(props);
+    firebase.auth().useDeviceLanguage();
+  }
+
+  userSignUp(email, password) {
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch((error) => {
+      // Handle Errors here.
+      if (!error) {
+        
+      } else {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      }
+    });
+  }
+
+
+  userSignIn(email, password) {
+    firebase.auth().signInWithEmailAndPassword(email, password).catch((error) => {
+      // Handle Errors here.
+      if (!error)
+      {
+        
+      } else {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+      }
+    });
+  }
+
   render(){
+    
     return(
       <div>
-        
+        <div type="button">
+          Registration
+        </div>
+        <div type="button">
+          Login
+        </div>
+        <div type="button">
+          Google
+        </div>
+        {/* <div type="button">
+          Login Vk
+        </div> */}
       </div>
     )
   }
--- client/components/Message.js
+++ client/components/Message.js
@@ -18,6 +18,7 @@ class Message extends React.Component{
           </div>
         </div>
       </div>
+    )
   }
 };
 
